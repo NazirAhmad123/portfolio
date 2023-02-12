125d826c438cf5ceeb4e1dbe149bf6ba55cb8ebb
Nazir <nazirahmadsaidzada@gmail.com>
2023-02-13 00:10:52 +0800

    Update email.php

--- email.php
+++ email.php
@@ -1,27 +1,18 @@
 <?php
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $to = "iamnaziir@gmail.com";
+  $subject = "Website Contact Form Submission";
+  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+  $headers = "From: $email\r\n";
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $to = 'iamnaziir@gmail.com';
-    $subject = 'New Message from Contact Form';
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-    $body = "<p>New Message from Contact Form:</p>";
-    $body .= "<p>Name: $name</p>";
-    $body .= "<p>Email: $email</p>";
-    $body .= "<p>Message: $message</p>";
-
-    if (mail($to, $subject, $body, $headers)) {
-        echo 'Your message was sent successfully.';
-    } else {
-        echo 'There was a problem sending your message. Please try again.';
-    }
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Form submitted successfully!";
+  } else {
+    echo "Failed to submit form. Please try again later.";
+  }
+} else {
+  echo "Failed to submit form. Please fill in all fields.";
 }
-
-?>
